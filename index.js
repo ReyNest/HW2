f67cc3db85de272dbf0e6c3a25f72de09f857b1b
Andrey PC <rey83@bk.ru>
2023-01-21 21:41:18 +1000

    Added file index.html

---
+++ index.js
@@ -0,0 +1,62 @@
+/*let a;
+a = 10;
+alert(`10`);
+alert(`20`)
+
+let relis;
+relis = `Год выпуска первого iPhone`
+alert(2007)
+
+let Name;
+Name = `Мультипарадигменный язык программирования`;
+alert(Name);
+
+let Number;
+Number = 10
+Number = 2;
+let result = 10 + 2;
+alert(result);
+
+let Number1;
+Number = 10
+Number = 2;
+result = 10 - 2;
+alert(result);
+
+let Number2;
+Number = 10
+Number = 2;
+result = 10 * 2;
+alert(result);
+
+let Number3;
+Number = 10
+Number = 2;
+result = 10 / 2;
+alert(result);
+
+let Number4;
+Number = 2
+Number = 5;
+result = 2 ** 5;
+alert(result);
+
+let Number5;
+a = 9
+b = 2;
+result = 9 % 2;
+alert(result);
+
+let num = 1
+num = ++num + 5;
+num = --num - 3;
+num = num * 7;
+num = num / 3;
+++num;
+--num;
+alert(num)
+*/
+let age = prompt('Сколько вам лет?');
+alert(result);
+
+
