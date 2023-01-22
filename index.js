4b6e2e33b80dcb2f1528ec9e0b41264aa424d8c1
Andrey PC <rey83@bk.ru>
2023-01-23 01:45:50 +1000

    Added file index.html

--- index.js
+++ index.js
@@ -1,62 +1,68 @@
-/*let a;
+let a;
 a = 10;
 alert(`10`);
-alert(`20`)
+alert(`20`);
 
 let relis;
-relis = `Год выпуска первого iPhone`
-alert(2007)
+relis = `Год выпуска первого iPhone`;
+alert(`${relis} 2007год`);
 
 let Name;
 Name = `Мультипарадигменный язык программирования`;
-alert(Name);
+alert(`JS- ${Name}`);
 
 let Number;
-Number = 10
-Number = 2;
-let result = 10 + 2;
+NumberOne = 10;
+NumberTwo = 2;
+result = 10 + 2;
 alert(result);
-
-let Number1;
-Number = 10
-Number = 2;
 result = 10 - 2;
 alert(result);
-
-let Number2;
-Number = 10
-Number = 2;
 result = 10 * 2;
 alert(result);
-
-let Number3;
-Number = 10
-Number = 2;
 result = 10 / 2;
 alert(result);
 
 let Number4;
-Number = 2
+Number = 2;
 Number = 5;
 result = 2 ** 5;
 alert(result);
 
 let Number5;
-a = 9
+a = 9;
 b = 2;
 result = 9 % 2;
 alert(result);
 
-let num = 1
+let num = 1;
 num = ++num + 5;
 num = --num - 3;
 num = num * 7;
 num = num / 3;
 ++num;
 --num;
-alert(num)
-*/
+alert(num);
+
 let age = prompt('Сколько вам лет?');
-alert(result);
+alert(`Ваш возраст: ` + age);
+/*
+const user = {
+    name: 'Василий',
+    age: '30',
+    isAdmin: 'true',
+}
+let name = String('Василий');
+age = Number(30);
+let isAdmin = Boolean('true');
+user.cityOfRresidence = 'Владивосток'; // "Добавление свойства объекту"
+user.age = 'height'; // "Изменение свойства объекту"
+delete user.cityOfRresidence; // "Удаление свойства объекта"
+*/
+let infoName = prompt(`Ваше имя?`);
+infoAge = prompt(`Ваш возраст`);
+alert(`Ваше имя: ${infoName}, Ваш возраст: ${infoAge}`);
 
-
+let userName = prompt('Как ваше имя?');
+alert(`Ваше имя: ${userName}`);
+alert(`Привет ${userName}!`);
