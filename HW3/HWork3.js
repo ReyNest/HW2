a035eaf3df47c4074bfc3e9078504b95937b6d85
Andrey PC <rey83@bk.ru>
2023-01-27 20:00:06 +1000

    added file index.js

---
+++ HW3/HWork3.js
@@ -0,0 +1,28 @@
+/*let password;
+password = prompt(`Введите пароль.`);
+
+if (password === 'Sova20') {
+    console.log('Пароль введен верно');    
+} else{
+    console.log('Пароль введен не правильно');
+}
+
+let c = prompt('Введите число');
+c <= 0;
+if (c >= 0 && c <= 10) {
+    console.log('Верно');
+} else{
+    console.log('Не верно');
+}
+*/
+//Задание  3
+
+let Number = prompt('Введите число');
+let d = 105;
+let e = 50;
+if (d <= 100 || e <= 100){
+    console.log('Верно');
+} else{
+    console.log('Не верно');
+}
+
